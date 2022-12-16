d80d2d0ca330311a89ffb7c4d06776607147f400
12-Davance <dawitderebie@gmail.com>
2022-12-16 15:42:09 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('cors');
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(morgan('combined'));
+app.use(express.json());
+
+module.exports = app;
